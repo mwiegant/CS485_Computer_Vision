4b64f4464ede2908f1c25c0f87393b39635b625c
Max Wiegant <mwiegant@bombora.com>
2017-04-17 15:24:13 -0700

    latest, writing an image with the threshold and superimposed circles

--- PA3/main.cpp
+++ PA3/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char** argv)
   int **fileImageData;
   int **thresholdedData;
   int **circleImageData;
+  int **circleImageThresholded;
   vector<Circle> circles;
 
   // check for correct amount of command line arguments
@@ -50,28 +51,41 @@ int main(int argc, char** argv)
 
   // DEBUG - print circle data
   cout << "DEBUG - found " << circles.size() << " circles." << endl;
-//  string text;
-//  for(int i = 0; i < circles.size(); i++) {
-//    text = "Circle......... x: " + to_string(circles[i].x) + ", y: " + to_string(circles[i].y) + ", r: " + to_string(circles[i].r);
-//    cout << text << endl;
-//  }
+  string text;
+  for(int i = 0; i < circles.size(); i++) {
+    text = "Circle......... x: " + to_string(circles[i].x) + ", y: " + to_string(circles[i].y) + ", r: " + to_string(circles[i].r);
+    cout << text << endl;
+  }
 
-  // draw detected circles onto an output image
+  // draw detected circles over the image from file
   for(int i = 0; i < circles.size(); i++) {
     if(i == 0) {
-      circleImageData = drawGreyscaleCircle(thresholdedData, numRows, numColumns, circles[i].x, circles[i].y, circles[i].r, 128);
+      circleImageData = drawGreyscaleCircle(fileImageData, numRows, numColumns, circles[i].x, circles[i].y, circles[i].r, 128);
     } else {
       circleImageData = drawGreyscaleCircle(circleImageData, numRows, numColumns, circles[i].x, circles[i].y, circles[i].r, 128);
     }
   }
 
+  // draw detected circles over the thresholded image
+  for(int i = 0; i < circles.size(); i++) {
+    if(i == 0) {
+      circleImageThresholded = drawGreyscaleCircle(thresholdedData, numRows, numColumns, circles[i].x, circles[i].y, circles[i].r, 128);
+    } else {
+      circleImageThresholded = drawGreyscaleCircle(circleImageThresholded, numRows, numColumns, circles[i].x, circles[i].y, circles[i].r, 128);
+    }
+  }
+
   // write thresholded image to file
-  fileOut = "images/threshold=" + to_string(threshold) + "_output.pgm";
+  fileOut = "images/image_threshold=" + to_string(threshold) + "_output.pgm";
   WritePGMImage(fileOut.c_str(), thresholdedData, numRows, numColumns, maxGreyValue);
 
-  // write image with circles to file
-  fileOut = "images/image_circles.pgm";
+  // write image with circles superimposed to file
+  fileOut = "images/image_superimposed.pgm";
   WritePGMImage(fileOut.c_str(), circleImageData, numRows, numColumns, maxGreyValue);
+
+  // write thresholded image with circles superimposed to file
+  fileOut = "images/image_threshold_superimposed.pgm";
+  WritePGMImage(fileOut.c_str(), circleImageThresholded, numRows, numColumns, maxGreyValue);
 
   cout << "INFO - Successfully completed program." << endl;
 
